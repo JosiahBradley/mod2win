3fe400faaba1513d9c674f8febc622c6b45a12c5
Josiah Bradley <jbradley@linkedin.com>
2019-10-17 20:58:34 -0700

    Document Mod 1

--- src/mod2win/mods/level1.py
+++ src/mod2win/mods/level1.py
@@ -1,3 +1,110 @@
+"""
+Welcome to level 1 mod!
+
+Exploration of variables is your goal in this mod file.
+
+A variable is a type of storage that a program uses to save values. Think of them as a box you can put things into.
+Sometimes the box can hold certain things and sometimes it is better for other items. This concept in programming is
+called typing or variable type.
+
+Variables have names and that is how we refer to them when we want to get their contents.
+
+A name can be a word that describes what you want your variable to do.
+
+Example variable names:
+ * age
+ * school
+ * grade
+ * my_name
+ * favorite_color
+ * really_long_variable_name_that_you_should_avoid_but_still_be_descriptive
+
+After you give your variable a name you need to assign it a value. We do this Python using the = sign
+
+Example assignment:
+ * age = 32
+ * school = "Barry University"
+ * grade = 16
+ * my_name = "Josiah Bradley"
+ * favorite_color = "Green"
+ * really_long_variable_name_that_you_should_avoid_but_still_be_descriptive = None
+
+Some types you may run into are numbers, or words, or special combinations of these.
+
+Number types include integers and floating point numbers (think decimals)
+
+In Python we call integers: int
+and we call decimal numbers: float
+
+Example int:
+ * a = 1
+ * b = 2000
+ * foo = 1234567890987654321
+
+Example float:
+ * temperature = 98.6
+ * money = 10.51
+ * pi = 3.141592653589793
+
+Words in Python are called strings, or str.
+Strings can even contain numbers or symbols.
+When we assign these variables we need a way for Python to understand the words.
+This is done by using a matching pair of quotes: ' or "
+Either can be used as long as they surround the word(s) and match.
+
+Example str:
+ * name = "Clifford"
+ * size = 'Big'
+ * color = "Red"
+ * pet = 'Dog'
+ * book = "Title of really cool book"
+ * password = '4$5gsuRhnj7asi!'
+
+Sometimes we want to collect multiple numbers or words at the same time.
+We have four ways to do this in Python, each with their own unique properties.
+
+Lists, Tuples, Sets, and Dictionaries.
+
+Lists are like a shopping list, they have a bunch of items you want to use for one purpose.
+You can store numbers or words in lists and they can grow in size to hold more variables.
+You can even put list inside of list to create complex structures, but we will go over that later.
+
+Lists can be created using [] or list()
+
+Example List:
+ * numbers = [ 1, 2, 3 ]
+ * letters = [ 'A', 'B', 'C' ]
+ * words = [ "dog", "cat", "turtle" ]
+ * tic_tac_toe = [ [ 'X', 'O', 'X' ], [ 'O', 'X', 'O'], [ 'O', 'X', 'O' ] ]
+
+Tuples are special types of list that can't change size or their contents. This is useful for
+defining things like colors or points on a graph.
+
+Tuples can be created using () or tuple()
+
+Example tuple:
+ * location = (3, 8)
+ * sky_blue = (135, 206, 235)
+ * best_words ('Python', 'Programming', 'Game')
+
+Sets and Dictionaries are more advances and will be used in later modules.
+
+You'll have access to the following variables:
+
+  * speed: Number (int or float)
+  * water: Number (int or float)
+  * color: tuple of 3 numbers between 1 and 255 like (1, 2, 3)
+
+You'll need to manipulate these to find out what they do.
+
+Ready, SET, go!
+"""
+
+
+# class is just a named collection of variables and special actions we call functions.
+# You'll learn more in the next module about functions.
 class Mod:
-    speed = 50
-    water = 1
+    # pass in Python means just pass me and do the next thing.
+    # It'll come in handy in later modules, but for now it's just a place holder.
+    # Feel free to delete it and put your variables here.
+    pass
